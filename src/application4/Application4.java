0548333c75a31c7433f279d54b68039f9713c163
antrohebsiba <antrohebsibaxe809@gmail.com>
2023-07-28 02:23:00 -1200

    build file

---
+++ src/application4/Application4.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application4;
+
+/**
+ *
+ * @author ELCOT
+ */
+public class Application4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=67,b=60,c=43,d=98;
+        int total=a+b+c-d;
+        System.out.println(total);
+        System.out.println(a+b);
+        System.out.println(a*c);
+        System.out.println(a/d);
+        System.out.println(a*c*d);
+        System.out.println((a+b)*d);
+        System.out.println((a-c)+b);
+        
+                
+        
+        
+       
+        
+        
+        
+        
+      
+        
+       
+        
+    }
+    
+}
